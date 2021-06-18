18ee298996864bea209bacdd0cf82a3ad5a9768f
Lauren Harmon <harmon.lauren@hotmail.com>
2021-06-18 17:50:31 -0500

    initial

---
+++ app.js
@@ -0,0 +1,29 @@
+//Express
+const express = require("express");
+const app = express();
+//Mongoose
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("database connected");
+});
+//Path and Views Settings
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//Models
+const Campground = require("./models/campground");
+///////////////////////////////////////////////////////////////////////
+app.listen(3000, () => {
+    console.log("Good to go!");
+})
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
