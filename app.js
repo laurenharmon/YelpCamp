0a7ad90d5ec36f66d294799a33f259a7b999d565
Lauren Harmon <harmon.lauren@hotmail.com>
2021-06-21 14:52:08 -0500

    CRU basics

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ db.once("open", () => {
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+//Parse Request Body
+app.use(express.urlencoded({ extended: true }));
+//Allow for Method Overrides
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 //Models
 const Campground = require("./models/campground");
 ///////////////////////////////////////////////////////////////////////
@@ -24,6 +29,47 @@ app.listen(3000, () => {
     console.log("Good to go!");
 })
 
+//HOME
 app.get("/", (req, res) => {
     res.render("home");
 })
+
+//CAMPGROUND INDEX
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+})
+
+//CAMPGROUND CREATE
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+})
+
+app.post("/campgrounds/:id", async (req, res) => {
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp.id}`);
+})
+
+//CAMPGROUND READ
+app.get("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render("campgrounds/show", { camp })
+})
+
+
+//CAMPGROUND UPDATE
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render("campgrounds/edit", { camp })
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${camp.id}`);
+})
+
+//CAMPGROUND DELETE
