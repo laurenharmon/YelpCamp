270b02bed3fccb3616f384d457838029b2c5140b
Lauren Harmon <harmon.lauren@hotmail.com>
2021-07-08 16:05:58 -0500

    multer for image file upload

--- schemas.js
+++ schemas.js
@@ -12,7 +12,7 @@ module.exports.campValidationSchema = Joi.object({
 
 module.exports.reviewValidationSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
+        rating: Joi.number().required().min(0).max(5),
         body: Joi.string().required()
     }).required()
 });
